3207f75862b2b278702db1ae37e8baaa12cd5196
Shariq12345 <shariqshaikh11042004@gmail.com>
2024-07-22 18:37:28 +0000

    Fixed bugs

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { columns, WeightColumn } from "./columns";
+import { columns, ProductColumn } from "./columns";
 import { useParams, useRouter } from "next/navigation";
 import Heading from "@/components/Heading";
 import { Button } from "@/components/ui/button";
@@ -11,31 +11,31 @@ import { DataTable } from "@/components/data-table";
 import ApiList from "@/components/api-list";
 
 type Props = {
-  data: WeightColumn[];
+  data: ProductColumn[];
 };
 
-const WeightClient = ({ data }: Props) => {
+const ProductClient = ({ data }: Props) => {
   const router = useRouter();
   const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Weights (${data.length})`}
-          description="Manage weights for your store."
+          title={`Products (${data.length})`}
+          description="Manage products for your store."
         />
-        <Button onClick={() => router.push(`/${params.storeId}/weights/new`)}>
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
           <PlusIcon className="mr-2 h-4 w-4" />
           Add New
         </Button>
       </div>
       <Separator />
       <DataTable columns={columns} data={data} searchKey="name" />
-      <Heading title="API" description="API calls for Weight" />
+      <Heading title="API" description="API calls for Products" />
       <Separator />
-      <ApiList entityName="weights" entityIdName="weightId" />
+      <ApiList entityName="products" entityIdName="productId" />
     </>
   );
 };
 
-export default WeightClient;
+export default ProductClient;
